c258456ac55a3817eecc765d006f5c3bc4043614
= <=>
2025-07-14 22:40:48 +0200

    Dodano enpointy użytkowników, rozpoczeto prace nad routerami

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException
 from typing import Annotated
-from passlib.context import CryptContext
 
+from routers.auth import hash_password
 from database import Base, get_db, engine
 from models import Reservation, User
 from schema import NewReservation, AddUser, ReadUser
+from routers import auth
 
 app = FastAPI()
 db_dependency = Annotated[Session, Depends(get_db)]
 Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
 
 
 
@@ -23,7 +26,7 @@ async def read_all_reservations(db: db_dependency):
 async def read_single_reservation(reservation_id: int, db: db_dependency):
     single_reservation = db.query(Reservation).filter(Reservation.id == reservation_id).first()
     if not single_reservation:
-        return 'Reservation not found'
+        raise HTTPException(status_code=404, detail='Reservation not found')
     return single_reservation
 
 @app.post("/reservations")
@@ -50,4 +53,40 @@ async def update_reservation(reservation_id: int, new_reservation: NewReservatio
         reservation.last_name = new_reservation.last_name
         reservation.number_of_people = new_reservation.number_of_people
         reservation.date = new_reservation.date
+    db.commit()
+
+@app.post("/users")
+async def create_user(user: AddUser, db: db_dependency):
+    hashed_password = hash_password(user.password)
+    db_user = User(email=user.email,
+                   hashed_password=hashed_password,
+                   is_active=True,
+                   is_admin=False)
+    db.add(db_user)
+    db.commit()
+
+@app.get("/users/{email}")
+async def find_user(db: db_dependency, email: str):
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="Nie ma takiego użytkownika!")
+    return user
+
+
+def get_user_by_email(db: db_dependency, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+@app.get("/users")
+async def read_all_users(db:db_dependency):
+    users = db.query(User).all()
+    return users
+
+@app.post("/register", response_model=ReadUser)
+def register(user_data: AddUser, db: db_dependency):
+    user = get_user_by_email(db, user_data.email)
+    if user:
+        raise HTTPException(status_code=400, detail='Taki użytkownik już istnieje!')
+    hashed = hash_password(user_data.password)
+    new_user = User(email=user_data.email, hashed_password=hashed)
+    db.add(new_user)
     db.commit()
--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,12 @@
+from fastapi import APIRouter
 from passlib.context import CryptContext
 from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordRequestForm
+
+from schema import AddUser
+
+router = APIRouter()
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -17,7 +24,19 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 10
 
 def create_access_token(data: dict):
     to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        return None
+
+
+
+
 
 
 
